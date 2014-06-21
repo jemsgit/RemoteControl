e85061f80f1e0d839c2c5143f4ce2780bc9fa006
jemsgit <jemguns@gmail.com>
2014-06-21 22:17:30 +0400

    Run and Stop server

--- serv.py
+++ serv.py
@@ -3,6 +3,12 @@
 import socket, os
 from Tkinter import *
 
+PORT = 9092
+if_run = False
+TEXT_RUN = "Run Server"
+TEXT_STOP = "Stop Server"
+
+
 wind = Tk()
 a = []
 wind.title("Server")
@@ -10,31 +16,38 @@ wind.geometry("300x50")
 
 
 
-def runserv():
-    sock = socket.socket()
-    sock.bind(('', 9092))
-    sock.listen(1)
-    conn, addr = sock.accept()
-#    addr = str(addr)
-    addrnew = (addr[0:1], ":", addr[1:2])
+def toggle_server():
+    global if_run, sock, conn
+    if if_run == False:
+        b.configure(text = TEXT_STOP)
+        sock = socket.socket()
+        sock.bind(('', 9092))
+        sock.listen(1)
+        conn, addr = sock.accept()
+        addrnew = (addr[0:1], ":", addr[1:2])
 
-    print 'connected:', addr
-    w1 = Label(wind, fg = "red", justify = "right" , padx = 10, pady = 10, font = "Times", text = addrnew).pack(side="right")
+        print 'connected:', addr
+        print conn
+        w1 = Label(wind, fg = "red", justify = "right" , padx = 10, pady = 10, font = "Times", text = addrnew).pack(side="right")
 
-    while True:
-        data = conn.recv(1024)
-        a.append(data)
-        print a[-1]
-        if not data:
-            break
-        conn.send(data)
+        while True:
+            data = conn.recv(1024)
+            a.append(data)
+            print a[-1]
+            if not data:
+                break
+            conn.send(data)
+        if_run = True
 
+    else:
+        b.configure(text = TEXT_RUN)
+        conn.close();
+        if_run = False
+    return if_run
 
-
-
-b = Button(wind, text = "Run Server", command=runserv).place(x = 10, y = 10)
-
-
+b = Button(wind, text = TEXT_RUN, command=toggle_server)
+b.pack()
+b.place(x = 10, y = 10)
 wind.mainloop()
 
 os.system(a[0])
